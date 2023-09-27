b03b0d487b32b1ebb055adbe65bfbf71f2f1de7d
hyegyeong <harp12553@knou.ac.kr>
2023-09-27 09:43:38 +0900

    Changed the path used to send SMS data via post from 'sms' to 'callHistory/sms'

--- app/src/main/java/com/example/mysmsreadapp/SmsApi.kt
+++ app/src/main/java/com/example/mysmsreadapp/SmsApi.kt
@@ -6,6 +6,6 @@ import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface SmsApi {
-    @POST("sms")
+    @POST("callHistory/sms")
     fun saveSMSData(@Body smsList: MutableList<SMSData>): Call<Void>
 }
